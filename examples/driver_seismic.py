00b6bc3652c58896822c9a5882dcaf87351cf089
Marc Laberge <marc.laberge@ucalgary.ca>
2024-03-04 22:05:31 -0700

    Plot of the raw s-wave data as well as the Gauss-Legendre quadrante function

--- examples/driver_seismic.py
+++ examples/driver_seismic.py
@@ -1,2 +1,23 @@
 #Will contain code that generates analysis results
 #and generates any plots for section 5.1
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    #Loading velocity data from file
+    wave_data = np.loadtxt('data/s_wave_data.txt')
+    #Seperate data into time and velocity
+    t = wave_data[:,0]
+    v = wave_data[:,1]
+    #X axis will be (t)ime (seconds), Y axis will be (v)elocity (mm/s)
+
+    plt.plot(t, v, 'k-', label = 'S-Wave Arrivals', linewidth = 0.5)
+    plt.ylabel('Velocity (v) [mm/s]')
+    plt.xlabel('Time (t) [second]')
+    plt.title('Collected data for S-Wave Arrivals')
+    plt.legend()
+    plt.savefig('data/s_wave_data_figure.png')
+    plt.close('all')
+
+if __name__ == '__main__':
+    main()
--- src/goph420_lab01/integration.py
+++ src/goph420_lab01/integration.py
@@ -1,8 +1,9 @@
+import numpy as np
 
-
-def integration_newton(x,f,alg):
+def integrate_newton(x,f,alg):
     """
     Integration of sample points using Newton-Cotes rules
+
     Parameters:
     -----------
         x:
@@ -28,7 +29,39 @@ def integration_newton(x,f,alg):
             Float
 
     """
+    pass
 
+def integrate_gauss(f, lims, npts):
+    """
+    Integration from sample points using Gauss-Legendre quadrature
+
+    Parameters:
+    -----------
+        f:
+            Reference to a callable object
+        lims:
+            Object with a len of 2 that contains the upper and lower bounds (x=a and x=b)
+        npts:
+            (Optional) Int that gives the number of integration points to use
+                Possible values are 1,2,3,4,5 default will be 3
+
+    Raises:
+    -------
+        TypeError:
+            If f isn't callable
+        ValueError:
+            If lims doesn't have a len == 2
+        ValueError:
+            If lims[0] or lims[1] are not convertable to float
+        ValueError:
+            If npts is not [1,2,3,4,5]
+    
+    Return:
+    -------
+        Integral estimate:
+            Float
+    """
+    pass
 
 
 
